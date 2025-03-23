4c7a4db4a00f834c009dfb220bab41d744a5bef4
Nusret Yalçın <yalcnusret@gmail.com>
2025-03-23 22:25:42 +0300

    User Repository and user service added

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class UserManager:IUserService
+    {
+        private IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+        public IDataResult<List<OperationClaim>> GetClaims(User user)
+        {
+            return new SuccessDataResult<List<OperationClaim>>(_userDal.GetClaims(user));
+        }
+
+        public IResult Add(User user)
+        {
+            _userDal.Add(user);
+            return new SuccessResult();
+        }
+
+        public IDataResult<User> GetByEmail(string email)
+        {
+            return new SuccessDataResult<User>(_userDal.Get(p => p.Email == email));
+
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            return new SuccessDataResult<List<User>>(_userDal.GetList().ToList());
+        }
+    }
+}
---
+++ DataAccess/Concrete/EfUserDal.cs
@@ -0,0 +1,33 @@
+﻿using Core.DataAccess;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+
+    public class EfUserDal : EfEntityRepositoryBase<User, EfDbContext>, IUserDal
+    {
+        public EfUserDal(EfDbContext context) : base(context)
+        {
+        }
+
+        public List<OperationClaim> GetClaims(User user)
+        {
+
+                var result = from operationClaim in _context.OperationClaims
+                             join userOperationClaim in _context.UserOperationClaims
+                                 on operationClaim.Id equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == user.Id
+                             select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+                return result.ToList();
+
+        }
+    }
+}
