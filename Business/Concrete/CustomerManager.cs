894a51b596de8c0efd34ee0f9bba64180dbc5bc1
Nusret Yalçın <yalcnusret@gmail.com>
2025-03-23 17:58:50 +0300

    Filtering implemented, and an extension method  written to combine queries

--- Business/Concrete/CustomerManager.cs
+++ Business/Concrete/CustomerManager.cs
@@ -2,9 +2,11 @@
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using Core.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,9 +21,37 @@ namespace Business.Concrete
             _customerDal = customerDal;
         }
 
-        public List<Customer> GetListPagedCustomer()
+        public List<Customer> GetListPagedCustomer(CustomerFilterDto customerFilterDto)
         {
-            var result = _customerDal.GetList(orderBy:p=>p.OrderByDescending(c=>c.Id),enableTracking:false);
+
+            Expression<Func<Customer, bool>> predicate = p => true; // Başlangıçta tüm veriyi getir
+
+            if (!string.IsNullOrEmpty(customerFilterDto.Name))// Name filtresini kontrol ediyor
+                predicate = predicate == null //filtre oluşturulmamışsa oluşturuluyor
+                    ? c => c.FirstName.Contains(customerFilterDto.Name)
+                    : predicate.And(c => c.FirstName.Contains(customerFilterDto.Name));//filtre oluşturulmuşsa mevcut predicate'e yeni koşul ekliyor
+
+            if (!string.IsNullOrEmpty(customerFilterDto.Email))
+                predicate = predicate == null
+                    ? c => c.Email.Contains(customerFilterDto.Email)
+                    : predicate.And(c => c.Email.Contains(customerFilterDto.Email));
+
+            if (!string.IsNullOrEmpty(customerFilterDto.Region))
+                predicate = predicate == null
+                    ? c => c.Region.Contains(customerFilterDto.Region)
+                    : predicate.And(c => c.Region.Contains(customerFilterDto.Region));
+
+            if (customerFilterDto.StartDate.HasValue)
+                predicate = predicate == null
+                    ? c => c.RegistrationDate >= customerFilterDto.StartDate.Value
+                    : predicate.And(c => c.RegistrationDate >= customerFilterDto.StartDate.Value);
+
+            if (customerFilterDto.EndDate.HasValue)
+                predicate = predicate == null
+                    ? c => c.RegistrationDate <= customerFilterDto.EndDate.Value
+                    : predicate.And(c => c.RegistrationDate <= customerFilterDto.EndDate.Value);
+
+            var result = _customerDal.GetList(predicate,orderBy:p=>p.OrderByDescending(c=>c.Id),enableTracking:false);
             return result.ToList();
         }
 
